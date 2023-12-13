76dd6203bab97587214c3a1a2dfb3a79c6eb1b64
Sowmeya Venkatesh <92679604+Sowmeya-venkatesh@users.noreply.github.com>
2023-12-13 17:48:23 +0000

    newupdate

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Sowmeya Venkatesh",
+  email: "somweya.venk@gmail.com",
+  gitHub: "Sowmeya-venkatesh",
+  linkedIn: "sowmeya-venkatesh-93b248216",
 };
 
 const primaryColor = "#4E567E";
